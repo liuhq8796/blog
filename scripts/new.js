88004ee550d55934c258f7b04462ca0760d1eb20
Lucas Liu <100069204+liuhq8796@users.noreply.github.com>
2022-07-13 16:56:43 +0800

    Update new.js

--- scripts/new.js
+++ scripts/new.js
@@ -71,7 +71,7 @@ async function init() {
 
 > 本文参加了由 [公众号@若川视野](https://lxchuan12.gitee.io/) 发起的每周源码共读活动，点击[了解详情](https://juejin.cn/post/7079706017579139102)一起参与。
 
-> 这是源码共读的第${result.order}期，链接：${result.url}。`,
+> 这是源码共读的第 ${result.order} 期，链接：${result.url}。`,
       };
     }
 
