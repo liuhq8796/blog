600cd11b657efa4a1d1868ccb45d332977a633d7
Lucas Liu <100069204+liuhq8796@users.noreply.github.com>
2023-04-21 11:31:43 +0000

    refactor: 增加自动配置 sidebar 功能

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitepress'
+import sidebar from './sidebar'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -10,54 +11,11 @@ export default defineConfig({
 
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: '首页', link: '/' },
-      { text: '全部文章', link: '/articles/' },
-    ],
+    nav: [{ text: '全部文章', link: '/articles/' }],
 
     outlineTitle: '页面导航',
 
-    sidebar: [
-      {
-        text: 'JS',
-        link: '/articles/js/',
-        items: [
-          {
-            text: 'await-to-js 如何优雅的捕获 await 的错误',
-            link: '/articles/js/await-to-js-how-to-elegantly-catch-errors-from-await',
-          },
-        ],
-      },
-      {
-        text: 'CSS',
-        link: '/articles/css/',
-        items: [
-          {
-            text: 'BEM 与 Atomic CSS 方法论',
-            link: '/articles/css/bem-and-atomic-css-methodology',
-          },
-          {
-            text: '重置 box-sizing 的最佳实践',
-            link: '/articles/css/reset-box-sizing-best-practice',
-          },
-        ],
-      },
-      {
-        text: 'HTML',
-        link: '/articles/html/',
-        items: [],
-      },
-      {
-        text: 'Nginx',
-        link: '/articles/nginx/',
-        items: [
-          {
-            text: 'Nginx 配置中的 JavaScript',
-            link: '/articles/nginx/javascript-in-nginx-configuration',
-          },
-        ],
-      },
-    ],
+    sidebar,
 
     socialLinks: [{ icon: 'github', link: 'https://github.com/liuhq8796/blog' }],
 
